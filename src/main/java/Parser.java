ebc2d11941f88131039c4530194fba31906d6e8a
S4RPYK3IVl5 <S4RPYK3IVl5@users.noreply.github.com>
2019-07-18 16:06:12 +0400

    Last commit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -14,15 +14,25 @@ public class Parser {
 
     public static void main(String[] args) throws IOException {
 
-        Pattern pattern = Pattern.compile(" [0-9]+");
+        Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile(" [0-9]+|(подлинник)|(копия)");
+        // [0-9]+|(подлинник)|(копия)
+        // [0-9]+
         List<Integer> integerList = new LinkedList<>();
-        int n = 1;
+        List<String> documentType = new LinkedList<>();
+        Map<String, String> linksMap = new HashMap<>();
+        int n = 0;
         int count = 0;
 
-        PdfReader reader = new PdfReader("http://admission.sgmu.ru/sites/default/files/file/2019/List/2019-list-ped-budg.pdf");
+        linksMap.put("Лечебное дело", "http://admission.sgmu.ru/sites/default/files/file/2019/List/2019-list-lech-budg.pdf");
+        linksMap.put("Педиатрия", "http://admission.sgmu.ru/sites/default/files/file/2019/List/2019-list-ped-budg.pdf");
 
-        //Лечебное дело - http://admission.sgmu.ru/sites/default/files/file/2019/List/2019-list-lech-budg.pdf
-        //Педиатрия - http://admission.sgmu.ru/sites/default/files/file/2019/List/2019-list-ped-budg.pdf
+        System.out.println("Выберете список, который выхотите запарсить (Педиатрия, Лечебное дело) !!! Точно как указано в скобочках !!! :");
+        String key = scanner.nextLine();
+        String value = linksMap.get(key);
+        System.out.println(value);
+
+        PdfReader reader = new PdfReader(value);
 
         for (int i = 1; i <= reader.getNumberOfPages(); ++i) {
 
@@ -31,60 +41,84 @@ public class Parser {
             System.out.println(text);
 
             Matcher matcher = pattern.matcher(text);
-            while (matcher.find()) {
+            while (matcher.find())
                 arrayList.add(text.substring(matcher.start(), matcher.end()));
-            }
         }
 
         // удаляет номер студента
-        for (int i = 0; i < arrayList.size(); i += 4) {
+        for (int i = 0; i < arrayList.size(); i += 5)
             check(i);
 
-        }
+        showMessage("/////////////// Баллы - тип документа ///////////////");
 
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.print(arrayList.get(i) + " ");
             count++;
-            if (count%4 == 0)
+            if (count%5 == 0)
                 System.out.println();
         }
 
         count = 0;
-        n = 0;
-
         for (int i = 0; i < arrayList.size(); i++) {
-            n += Integer.valueOf(arrayList.get(i).trim());
             count++;
+            if (count == 5){
+                documentType.add(arrayList.get(i));
+                count = 0;
+                continue;
+            }
+            n += Integer.valueOf(arrayList.get(i).trim());
             if (count == 4){
                 //System.out.println((i/4+1) + " : " + n);
                 integerList.add(n);
                 n = 0;
-                count = 0;
             }
         }
 
-        count = 0;
+        showMessage("/////////////// Результат - тип документа ///////////////");
+
+        for (int i = 0; i < integerList.size(); i++) {
+            System.out.println(integerList.get(i) + " : " + documentType.get(i));
+        }
+
+        showMessage("/////////////// Отсортированные баллы ///////////////");
 
         Collections.sort(integerList);
         Collections.reverse(integerList);
 
+        count = 1;
         for (int i : integerList)
-        {
             System.out.println(count++ + " : " + i);
+
+        showMessage("/////////////// Число копий и подлинников ///////////////");
+
+        int countOfCopy = 0, countOfOriginal = 0;
+        for (String string : documentType) {
+            if (string.equals("копия"))
+                countOfCopy++;
+            else
+                countOfOriginal++;
         }
+        System.out.println("Копий: " + countOfCopy + ", Оригиналов: " + countOfOriginal);
+
+        showMessage("Если есть предлодения или вопросы, обращайтесь по адрессу электронной почты: andreysaprykin@mail.ru");
 
         reader.close();
     }
 
+    private static void showMessage(String message) {
+        System.out.println();
+        System.out.println(message);
+        System.out.println();
+    }
+
     private static void check(int i) {
         //У последнего не првоеряем
-        if (i+3 >= arrayList.size()) {
+        if (i+4 >= arrayList.size()) {
             arrayList.remove(i);
             return;
         }
 
-        if (Integer.valueOf(arrayList.get(i+2).trim()) <= 10) // П
-        {
+        if (Integer.valueOf(arrayList.get(i+1).trim()) <= 10) { // 0 экзаменов
             for (int j = 0; j < 3; j++)
                 arrayList.remove(i);
 
@@ -92,7 +126,8 @@ public class Parser {
             return;
         }
 
-        if (Integer.valueOf(arrayList.get(i+3).trim()) <= 10) { // без 1 экзамена
+        if (Integer.valueOf(arrayList.get(i+2).trim()) <= 10) // П
+        {
             for (int j = 0; j < 4; j++)
                 arrayList.remove(i);
 
@@ -100,8 +135,8 @@ public class Parser {
             return;
         }
 
-        if (Integer.valueOf(arrayList.get(i+1).trim()) <= 10) { // 0 экзаменов
-            for (int j = 0; j < 2; j++)
+        if (Integer.valueOf(arrayList.get(i+3).trim()) <= 10) { // без 1 экзамена
+            for (int j = 0; j < 5; j++)
                 arrayList.remove(i);
 
             check(i);
